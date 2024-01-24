405fe899067cd4d1fc94a0646f71c730f1c7ae9b
owgoldner <owen.goldner@gmail.com>
2024-01-24 16:25:44 -0500

    in-class work

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,12 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+
+function logMsg() {
+    console.log(`Error message: ${this.errMsg}`);
+}
+
+const o1 = {
+    val: 1,
+    errMsg: "error"
+}
+
+logMsg.call(o1);
