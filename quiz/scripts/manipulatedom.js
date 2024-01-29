8c3e3f481bdcd47f5922f1cbb8544a853efba484
owgoldner <owen.goldner@gmail.com>
2024-01-29 16:22:31 -0500

    in-class activity

--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -21,11 +21,28 @@ function createTxtNode(txt) {
   return txtNode;
 }
 
+function createButtonNode(txt, listener) {
+  let btnNode = document.createElement("button");
+  btnNode.appendChild(createTxtNode(txt));
+  btnNode.addEventListener('click', listener);
+  return btnNode;
+}
+
+function edit(cell) {
+  let inpNode = document.createElement("input");
+  inpNode.type = "text";
+  inpNode.placeholder = "Enter Cell (x,y) ...";
+  cell.replaceChild(inpNode, cell.childNodes[0]);
+}
+
 function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
     let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
-    tableNode.appendChild(createTRNode([col1]));
+    let col2 = createTDNode(createButtonNode("Edit text", function() {
+      edit(col1);
+    }));
+    tableNode.appendChild(createTRNode([col1, col2]));
   }
   document.getElementById("root").appendChild(tableNode);
 }
